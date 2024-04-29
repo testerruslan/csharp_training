353df7099bb9a43728eb9754659ed2c24b8a7291
avia007008 <avia007008@gmail.com>
2024-04-29 16:21:05 +0300

    lesson 2

---
+++ addressbook-web-tests/appmanager/NavigationHelper.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace WebAddressbookTests
+{
+    public class NavigationHelper : HelperBase
+
+    {
+               private string baseURL;
+
+        public NavigationHelper(ApplicationManager manager, string baseURL) : base(manager)
+        {
+                       this.baseURL = baseURL;
+        }
+
+    
+        public void GoToHomePage()
+        {
+            driver.Navigate().GoToUrl("http://localhost/addressbook/addressbook/");
+        }
+
+
+        public void GoToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+    }
+     
+}
--- addressbook-web-tests/model/AccountData.cs
+++ addressbook-web-tests/model/AccountData.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace WebAddressbookTests
 {
-    internal class AccountData
+    public class AccountData
     {
         private string username;
         private string password;
